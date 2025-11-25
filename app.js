0b01036048fcf8445083c0ed781a2cd4575e241d
Nursaadet <nursaadetozdemir@gmail.com>
2025-11-25 23:12:25 +0300

    Set up basic Express API with MongoDB connection and user authentication endpoints (register/login with bcrypt)

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+require("./src/db/dbConnection");
 const port = process.env.PORT || 5001;
+const router = require("./src/routers");
+
+//middlewares
+app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  express.urlencoded({ extended: true, limit: "50mb", parameterLimit: 50000 })
+);
+
+
+
+app.use("/api", router);
 
 app.get("/", (req, res) => {
   res.json({ message: "API is running" });
 });
+
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
